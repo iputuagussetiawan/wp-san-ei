5cec6bfa6f24cf658f5f53c972a6471f2954d78c
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2023-07-26 12:14:14 +0800

    Mega menu improvement

--- functions.php
+++ functions.php
@@ -6,7 +6,6 @@ require_once('functions/polylang.php');
 require_once('functions/script_enqueue.php');
 require_once('functions/WP_Bootstrap_Navwalker.php');
 require_once('functions/woocommerce.php');
-
 /**
  * Themesetup
  * Add menus configuration
@@ -15,69 +14,60 @@ require_once('functions/woocommerce.php');
  */
 function theme_setup()
 {
-  add_theme_support('menus');
-
-  register_nav_menu('primary', 'Primary Left Navigation');
-  register_nav_menu('secondary', 'Primary Right Navigation');
-  register_nav_menu('third', 'Footer Navigation');
-  register_nav_menu('fourth', 'Mobile Language Navigation');
-  register_nav_menu('top-nav', 'Top Navbar');
+    add_theme_support('menus');
+    register_nav_menu('primary', 'Primary Left Navigation');
+    register_nav_menu('secondary', 'Primary Right Navigation');
+    register_nav_menu('third', 'Footer Navigation');
+    register_nav_menu('fourth', 'Mobile Language Navigation');
+    register_nav_menu('top-nav', 'Top Navbar');
 }
 add_action('init', 'theme_setup');
-
 // AUTOMATICALLY ADD CATEGORY PRODUCT ON MENU
 function add_membership_in_menu($items, $args){
-  if ($args->theme_location == 'primary') {
-
-    $cat_args = array(
-        'number'     => 7,
-        'hide_empty' => false,
-        'parent' => 0
-    );
-
-    $product_categories = get_terms('product_cat', $cat_args);
-    $currentLang = pll_current_language();
-    $item = '<li class="menu-item dropdown nav-item has-megamenu order-3">';
-    if ($currentLang=='en') {
-      $productEN = get_field('product_categories_en', 'option');
-      $item .= '<a href="'. $productEN .'" data-toggle="dropdown" aria-expanded="false" class="dropdown-toggle nav-link"><span itemprop="name">Products</span></a>';
-    } elseif ($currentLang=='id') {
-      $productID = get_field('product_categories_id', 'option');
-      $item .= '<a href="'. $productID .'" data-toggle="dropdown" aria-expanded="false" class="dropdown-toggle nav-link"><span itemprop="name">Produk</span></a>';
-    }
-    
-    $item .= '<div class="dropdown-menu megamenu">';
-    $item .= '<div class="row justify-content-center align-items-center">';
-    foreach ($product_categories as $category) {
-      $thumbnail_id = get_term_meta( $category->term_id, 'thumbnail_id', true );
-      $image = wp_get_attachment_image_src( $thumbnail_id, 'large' );
-        $item .= '<div class="menu-item nav-item col-md-3">';
-        $item .= '<div class="img-zoom">';
-        $item .= '<a href="' . get_category_link($category->term_id) . '" class="dropdown-item">';
-        $item .= '<div class="img-ratio img-ratio__image-container">';
-        $item .= '<img class="img-fit dropdown-item__image" src="'. $image[0]. '" alt="'. $category->name .'">';
+    if ($args->theme_location == 'primary') {
+        $cat_args = array(
+            'number'     => 7,
+            'hide_empty' => false,
+            'parent' => 0
+        );
+        $product_categories = get_terms('product_cat', $cat_args);
+        $currentLang = pll_current_language();
+        $item = '<li class="menu-item dropdown nav-item has-megamenu order-3">';
+        if ($currentLang=='en') {
+            $productEN = get_field('product_categories_en', 'option');
+            $item .= '<a href="'. $productEN .'" data-toggle="dropdown" aria-expanded="false" class="dropdown-toggle nav-link"><span itemprop="name">Products</span></a>';
+        } elseif ($currentLang=='id') {
+            $productID = get_field('product_categories_id', 'option');
+            $item .= '<a href="'. $productID .'" data-toggle="dropdown" aria-expanded="false" class="dropdown-toggle nav-link"><span itemprop="name">Produk</span></a>';
+        }
+        $item .= '<div class="dropdown-menu megamenu">';
+            $item .= '<div class="megamenu__grid">';
+            foreach ($product_categories as $category) {
+                $thumbnail_id = get_term_meta( $category->term_id, 'thumbnail_id', true );
+                $image = wp_get_attachment_image_src( $thumbnail_id, 'large' );
+                $item .= '<div class="menu-item nav-item megamenu-item">';
+                $item .= '<a href="' . get_category_link($category->term_id) . '" class="dropdown-item megamenu-item__link">';
+                $item .= '<div class="megamenu-item__image-container">';
+                $item .= '<img class="megamenu-item__image" src="'. $image[0]. '" alt="'. $category->name .'">';
+                $item .= '</div>';
+                $item .= '<div class="megamenu-item__title">'. $category->name .'</div>';
+                $item .= '</a>';
+                $item .= '</div>';
+            }
+        $item .= '<div class="menu-item nav-item text-center megamenu-item">';
+        if ($currentLang=='en') {
+            $categoriPage = get_field('product_categories_en', 'option');
+            $item .= '<a href="' . $categoriPage . '" class="btn-standard">explore more</a>';
+        } elseif ($currentLang=='id') {
+            $categoriPageID = get_field('product_categories_id', 'option');
+            $item .= '<a href="' . $categoriPageID . '" class="btn-standard">selengkapnya</a>';
+        }
         $item .= '</div>';
-        $item .= '<div class="cat-title">'. $category->name .'</div>';
-        $item .= '</a>';
         $item .= '</div>';
         $item .= '</div>';
+        $item .= '</li>';
+        $items .= $item;
     }
-
-    $item .= '<div class="menu-item nav-item col-md-3 text-center">';
-    if ($currentLang=='en') {
-      $categoriPage = get_field('product_categories_en', 'option');
-      $item .= '<a href="' . $categoriPage . '" class="btn-standard">explore more</a>';
-    } elseif ($currentLang=='id') {
-      $categoriPageID = get_field('product_categories_id', 'option');
-      $item .= '<a href="' . $categoriPageID . '" class="btn-standard">selengkapnya</a>';
-    }
-    $item .= '</div>';
-
-    $item .= '</div>';
-    $item .= '</div>';
-    $item .= '</li>';
-    $items .= $item;
-  }
 
   // if ($args->theme_location == 'secondary') {
   //   $currentLang = pll_current_language();  
@@ -141,75 +131,72 @@ function add_membership_in_menu($items, $args){
   //   $item .=  '</li>';
   //   $items .= $item;
   // }
-  return $items;
-}add_filter('wp_nav_menu_items', 'add_membership_in_menu', 10, 2);
+    return $items;
+}
+add_filter('wp_nav_menu_items', 'add_membership_in_menu', 10, 2);
 
 
 // LOGO IN LOGIN ADMIN
 function my_login_logo() { ?>
-  <style type="text/css">
-    #login h1 a, .login h1 a {
-      background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/logo.png);
-      height: 65px;
-      width: 320px;
-      background-size: 200px 65px;
-      background-repeat: no-repeat;
-    }
-  </style>
+    <style type="text/css">
+        #login h1 a, .login h1 a {
+        background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/logo.png);
+        height: 65px;
+        width: 320px;
+        background-size: 200px 65px;
+        background-repeat: no-repeat;
+        }
+    </style>
 <?php }
 add_action( 'login_enqueue_scripts', 'my_login_logo' );
 
 
 // set url of login logo in login page  
 function tmdr_login_logo_url() {  
-  return home_url();  
+    return home_url();  
 } 
 add_filter( 'login_headerurl', 'tmdr_login_logo_url' ); 
 // custom login page title  
 function tmdr_custom_login_title($origtitle) {  
-  return get_bloginfo('name').' - Login'; 
+    return get_bloginfo('name').' - Login'; 
 } 
 add_filter('login_title', 'tmdr_custom_login_title', 99);
 
 
 //GET YOUTUBE ID
 function tmdr_youtube_id($url) {
-  preg_match('%(?:youtube(?:-nocookie)?.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu.be/)([^"&?/ ]{11})%i', $url, $match);
-  echo $match[1];
+    preg_match('%(?:youtube(?:-nocookie)?.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu.be/)([^"&?/ ]{11})%i', $url, $match);
+    echo $match[1];
 }
 
 
 // PROGRESSIVE MARKET
 function filter_projects() {
-  $catSlug = $_POST['category'];
-
-  $ajaxposts = new WP_Query([
-    'post_type'     => 'agent', 
-    'posts_per_page'  => -1,
-    'tax_query'     => array(
-      array(
-        'taxonomy'  => 'agent_location',
-        'field'   => 'slug',
-        'terms'   => $catSlug
-      )
-    )
-  ]);
-  $response = '';
-
-  if($ajaxposts->have_posts()) {
-    while($ajaxposts->have_posts()) : $ajaxposts->the_post();
-      $response .= get_template_part('templates/agent-list');
-    endwhile;
-  } else {
-    $response = '<div class="col-12">No agent</div>';
-  }
-
-  echo $response;
-  exit;
+    $catSlug = $_POST['category'];
+    $ajaxposts = new WP_Query([
+        'post_type'     => 'agent', 
+        'posts_per_page'  => -1,
+        'tax_query'     => array(
+        array(
+            'taxonomy'  => 'agent_location',
+            'field'   => 'slug',
+            'terms'   => $catSlug
+        )
+        )
+    ]);
+    $response = '';
+    if($ajaxposts->have_posts()) {
+        while($ajaxposts->have_posts()) : $ajaxposts->the_post();
+        $response .= get_template_part('templates/agent-list');
+        endwhile;
+    } else {
+        $response = '<div class="col-12">No agent</div>';
+    }
+    echo $response;
+    exit;
 }
 add_action('wp_ajax_filter_projects', 'filter_projects');
 add_action('wp_ajax_nopriv_filter_projects', 'filter_projects');
-
 
 // WOOCOMMERCE DISABLE SEARCH RESULT GO TO SINGLE PAGE
 add_filter( 'woocommerce_redirect_single_search_result', '__return_false' );
@@ -220,13 +207,12 @@ add_filter( 'woocommerce_registration_error_email_exists', function( $html ) {
     $url = add_query_arg( 'redirect_checkout', 1, $url );
     $html = str_replace( 'An account is already registered with your email address. Please log in.', 'An account is already registered with your email address. <a href="'.$url.'">Please log in.</a>', $html );
     return $html;
-} );
+});
 
 // set address street validation minlength = 10
 add_action('woocommerce_checkout_process', 'wh_addressStreetValidateCheckoutFields');
 function wh_addressStreetValidateCheckoutFields() {
     $billing_address = filter_input(INPUT_POST, 'billing_address_1');
-
     if (strlen(trim(preg_replace('/^[6789]\d{9}$/', '', $billing_address))) <= 10) {
         wc_add_notice(__('<strong>Billing Street address</strong> field has a too short input (min: 10).'), 'error');
     }
