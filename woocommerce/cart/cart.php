a3b4df5ca5d4a2adbabf5625f1a918900d4bcbb7
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2023-08-29 11:39:23 +0800

    adjust responsive page wishlist

--- woocommerce/cart/cart.php
+++ woocommerce/cart/cart.php
@@ -51,7 +51,7 @@ defined( 'ABSPATH' ) || exit; ?>
 								echo apply_filters( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 									'woocommerce_cart_item_remove_link',
 									sprintf(
-										'<a href="%s" class="remove" aria-label="%s" data-product_id="%s" data-product_sku="%s">&times;</a>',
+										'<a href="%s" class="remove" aria-label="%s" data-product_id="%s" data-product_sku="%s"></a>',
 										esc_url( wc_get_cart_remove_url( $cart_item_key ) ),
 										esc_html__( 'Remove this item', 'woocommerce' ),
 										esc_attr( $product_id ),
