8e2b5be69610c23e8f78f563c791e195fc9b6cd9
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2023-09-11 16:30:43 +0800

    improve navbar

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -2980,6 +2980,18 @@ searchToggle.addEventListener("click", function (event) {
   searchToggle.classList.toggle('close');
   searchField.focus();
 });
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll('.navbar .dropdown').forEach(function (everydropdown) {
+    everydropdown.addEventListener('shown.bs.dropdown', function () {
+      el_overlay = document.createElement('span');
+      el_overlay.className = 'screen-darken';
+      document.body.appendChild(el_overlay);
+    });
+    everydropdown.addEventListener('hide.bs.dropdown', function () {
+      document.body.removeChild(document.querySelector('.screen-darken'));
+    });
+  });
+});
 
 /***/ }),
 
